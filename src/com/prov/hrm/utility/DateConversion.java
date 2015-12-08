6e44bc4ed97859e477773a4caac09f6e55bae79f
surendarswo <surendarswo@gmail.com>
2015-12-08 10:38:13 +0530

    Checking that all data got comitted

---
+++ src/com/prov/hrm/utility/DateConversion.java
@@ -0,0 +1,18 @@
+package com.prov.hrm.utility;
+
+import java.text.SimpleDateFormat;
+
+public class DateConversion 
+{
+	public static void ymdTodmy(String indate)
+	{
+		SimpleDateFormat sdinput = new SimpleDateFormat("dd-MM-yyyy");
+		
+	}
+	public static void dmyToymd(String indate)
+	{
+		SimpleDateFormat sdinput = new SimpleDateFormat("yyyy-MM-dd");
+		
+	}
+
+}
